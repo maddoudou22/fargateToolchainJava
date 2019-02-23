a74e858c9b7922115c4c1e5a0e72178c2ee50e62
user <you@example.com>
2019-02-23 18:28:59 +0100

    latest push from windows

--- Git Push.bat
+++ Git Push.bat
@@ -1,9 +1,9 @@
 
 echo off
 
-SET nom_Projet_Git=serverlessEFSToolchainJava
-SET URL_Projet_Git=https://github.com/maddoudou22/serverlessEFSToolchainJava.git
-SET Repertoire_Local_Projet=C:\awsLambda\java\serverlessEFSToolchainJava
+SET nom_Projet_Git=fargateToolchainJava
+SET URL_Projet_Git=https://github.com/maddoudou22/fargateToolchainJava.git
+SET Repertoire_Local_Projet=C:\awsLambda\java\fargateToolchainJava
 
 cd %Repertoire_Local_Projet%
 
